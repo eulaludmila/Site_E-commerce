439a659997421ea8ffec65f1d426123901f1d932
Eula <eula.silva.teixeira@gmail.com>
2020-12-13 22:22:10 -0300

    09 - Ajustes propriedades do input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,7 +3,7 @@ import { GroupInput } from './styled'
 const Input = (props) => (
 
     <GroupInput>
-        <input className={props.erro ? 'invalido' : ''} placeholder={props.placeholder} name={props.name} value={props.value} className={props.erro ? 'invalido' : ''} onChange={props.onChange} />
+        <input placeholder={props.placeholder} name={props.name} value={props.value} className={props.erro ? 'invalido' : ''} onChange={props.onChange} />
         {props.erro && <span>{props.textoErro}</span>}
     </GroupInput>
 
